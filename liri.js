7683cacb65a8bc3ed3650cd0e4a1ed41bd5a990f
zachmilo <zachmilo45@gmail.com>
2017-04-26 21:06:58 -0400

    saving state

--- liri.js
+++ liri.js
@@ -2,32 +2,33 @@ var keys = require("./keys.js");
 var Twitter = require("twitter");
 var spotify = require("spotify");
 var request = require('request');
-var fs = require("fs")
+var fs = require("fs");
 
 var client = new Twitter(keys.twitterKeys);
 
 var action = process.argv[2];
 
-switch(action) {
-    case "my-tweets":
-        tweets();
-        break;
-    case "spotify-this-song":
-        var song = process.argv[3];
-        searchSong(song);
-        break;
-    case "movie-this":
-        var movie = process.argv[3];
-        searchMovie();
-        break;
-    case "do-what-it-says":
-        var doIt = process.argv[3];
-        doIt(doIt);
-        break;
-    default:
-        console.log("Please check the input case was not found");
-}
+command(action);
 
+function command(action) {
+
+    switch(action) {
+        case "my-tweets":
+            tweets();
+            break;
+        case "spotify-this-song":
+            searchSong();
+            break;
+        case "movie-this":
+            searchMovie();
+            break;
+        case "do-what-it-says":
+            doIt();
+            break;
+        default:
+            console.log("Please check the input case was not found");
+    }
+}
 function tweets() {
 
     client.get("statuses/home_timeline","trim_user=true")
@@ -45,10 +46,9 @@ function tweets() {
       })
 }
 
-function searchSong(song) {
+function searchSong() {
 
-    var track = song || "The Sign";
-
+    var song = multiParam() || "The Sign";
     spotify.search({ type: "track", query: song }, function(err, data) {
         if (err) {
             console.log('Error occurred: ' + err);
@@ -64,20 +64,46 @@ function searchSong(song) {
     });
 }
 
-function searchMovie(movie) {
-
+function searchMovie() {
+    var movie = multiParam();
     movieSplit = movie.trim().split(" ");
-    var formatQuery = "";
-    for(var word=0; word < movieSplit.length-1; word++) {
-        formatQuery += movieSplit[word]+"-";
+    var formatQuery = ""
+    if(movieSplit.length > 1) {
+        for(var word = 0; word < movieSplit.length-1; word++) {
+            formatQuery += movieSplit[word]+"-";
+        }
     }
-    console.log(formatQuery);
-    request("http://www.omdbapi.com/?t=+"formatQuery"+&tomatoes=true&r=json", function (error, response, body) {
-    console.log('statusCode:', response); // Print the response status code if a response was received
- // console.log('body:', body); // Print the HTML for the Google homepage.
-});
+    else
+    {
+        formatQuery = movieSplit[0];
+    }
+    request("http://www.omdbapi.com/?t="+formatQuery+"&tomatoes=true&r=json", function (error, response, body) {
+        var result = JSON.parse(body);
+        console.log("* Title: "+result.Title);
+        console.log("* Year of Release: "+result.Year);
+        console.log("* Country: "+result.Country);
+        console.log("* Language: "+result.Language);
+        console.log("* Actors: "+result.Actors);
+        console.log("* Rotten Tomatoes: "+result.tomatoUrl);
+    });
 }
+function multiParam() {
+    var builtParam ="";
+    if(process.argv.length < 4) {
+        builtParam = process.argv[3];
+    }
 
+    for (var param = 3; param < process.argv.length; param++) {
+        builtParam+=process.argv[param]+" ";
+    }
+    //console.log(builtParam);
+    return builtParam;
+}
 function doIt() {
+    fs.readFile('random.txt', 'utf8', function(error, data){
 
+        var format = data.trim().split(",");
+        process.argv.push(format[1]);
+        command(format[0]);
+    });
 }
